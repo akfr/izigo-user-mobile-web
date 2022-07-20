4ff8a7502732743848844756106c59a0837c8b80
Freddy Ahouassou <freddy.ahouassou@gmail.com>
2022-07-20 16:38:00 -0400

    Change database config

--- config.php
+++ config.php
@@ -1,14 +1,14 @@
 <?php
 // Informations d'identification
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_SERVER', 'izigo-admin.cznysp3sbdky.us-east-2.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'N5w5CKEM8xL3zK2KePKv8HkbFp6y4sAG');
 define('DB_NAME', 'delivery_app_db');
  
-// Connexion � la base de donn�es MySQL 
+// Connexion a la base de donnees MySQL 
 $con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
-// V�rifier la connexion
+// Verifier la connexion
  if($con === false){
   
       die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
